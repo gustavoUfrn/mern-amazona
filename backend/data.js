2b4e2d299a459fd06767103ce0931bd6972ecdad
gustavoufrn <mxgugaa@gmail.com>
2025-05-26 11:07:44 -0300

    .

--- backend/data.js
+++ backend/data.js
@@ -1,6 +1,7 @@
 export const data = {
   products: [
     {
+      _id: '1',
       name: 'Nike Slim Shirt',
       slug: 'nike-slim-shirt',
       category: 'shirts',
@@ -13,6 +14,7 @@ export const data = {
       description: 'high quality shirt',
     },
     {
+      _id: '2',
       name: 'Adidas Fit Shirt',
       slug: 'adidas-fit-shirt',
       category: 'shirts',
@@ -25,6 +27,7 @@ export const data = {
       description: 'high quality shirt',
     },
     {
+      _id: '3',
       name: 'Nike Slim Pant',
       slug: 'nike-slim-pant',
       category: 'Pants',
@@ -37,6 +40,7 @@ export const data = {
       description: 'high quality Product',
     },
     {
+      _id: '4',
       name: 'Adidas Fit Pant',
       slug: 'adidas-fit-pant',
       category: 'Pants',
--- backend/src/server.js
+++ backend/src/server.js
@@ -17,6 +17,16 @@ server.get('/api/products/slug/:slug', (req, res) => {
   }
 });
 
+server.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+
+  if (product) {
+    return res.send(product);
+  } else {
+    return res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`Server is running on localhost://${port}`);
